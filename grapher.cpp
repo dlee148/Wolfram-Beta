3ffda03a5b0cf4d5cfd2f33def7ae21ff8e21aa3
Daniel Lee <danieldavidlee@hotmail.com>
2015-12-10 18:26:49 -0800

    Filled graph with arbitrary characters

--- grapher.cpp
+++ grapher.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+using namespace std;
+
+void fillGraph(char graph[][50]);
 
 int main() {
-    
+    char graph[50][50];
+    fillGraph(graph);
+    for (int i = 0; i < 50; ++i) {
+        for (int j = 0; j < 50; ++j) {
+            cout << graph[i][j];
+        }
+        cout << endl;
+    }
+}
+
+void fillGraph(char graph[][50]) {
+    for (int i = 0; i < 50; ++i) {
+        for (int j = 0; j < 50; ++j) {
+            graph[i][j] = '*';
+        }
+    }
 }
