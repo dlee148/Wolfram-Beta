39e445f6deeca040e083497f74f5f7ec983bb187
Daniel Lee <danieldavidlee@hotmail.com>
2015-12-11 22:39:15 -0800

    Rewrote everything with an object oriented approach, implemented a bunch of new stuff

--- wolfram-beta.cpp
+++ wolfram-beta.cpp
@@ -1,74 +1,174 @@
+/*
+ =========================================================
+ 
+ Things to implement:
+ 
+ 1. Create graph
+ 2. Scale graph according to output
+ 
+ =========================================================
+ 
+ Eventual:
+ Dealing with parentheses
+ Logarithmic functions
+ Trig functions
+ Compute derivatives and integrals
+ 
+ =========================================================
+*/
+
+const int MAX_TERMS = 10;
+
 #include <iostream>
 #include <string>
 #include <cmath>
 using namespace std;
 
-void fillGraph(char graph[][51]);
-void drawAxes(char graph[][51]);
-void getCoefficients(int coefficients[], int degree);
-void draw(int coefficients[], int degree, char graph[][51]);
+/*
+ ======================================
+ Class definitions and member functions
+ ======================================
+*/
+
+// Term class definition and member functions
+
+class Term {
+public:
+    // Constructor
+    Term(double degree, double coeff);
+    //Accessor
+    double degree();
+    double coeff();
+private:
+    double m_degree;
+    double m_coeff;
+};
+
+Term::Term(double degree, double coeff) {
+    m_degree = degree;
+    m_coeff =  coeff;
+}
+
+double Term::degree() {
+    return m_degree;
+}
+
+double Term::coeff() {
+    return m_coeff;
+}
+
+// Function class definition and member functions
+
+class Function {
+public:
+    // Constructor/destructor
+    Function();
+    ~Function();
+    //Mutators
+    void takeInput();
+    void calculateValues();
+    //Accessors
+    Term* terms(int index);
+    double* values();
+    int nTerms();
+    void displayFunction();
+private:
+    Term* m_terms[MAX_TERMS];
+    double m_values[51];
+    int m_userTerms;
+    int m_nTerms;
+};
+
+Function::Function() {
+    m_nTerms = 0;
+    cout << "How many terms are in your function? ";
+    cin >> m_userTerms;
+    takeInput();
+    calculateValues();
+}
+
+Function::~Function() {
+    for (int i = 0; i < m_nTerms; ++i) {
+        delete m_terms[i];
+    }
+}
+
+void Function::takeInput() {
+    for (int i = 0; i < m_userTerms; ++i) {
+        double degree = 0;
+        double coeff = 0;
+        
+        cout << "What is the degree of term " << i + 1 << "? ";
+        cin >> degree;
+        
+        cout << "What is the coefficient of term " << i + 1 << "? ";
+        cin >> coeff;
+        
+        if (coeff != 0) {
+            m_terms[i] = new Term(degree, coeff);
+            ++m_nTerms;
+        }
+    }
+}
+
+void Function::calculateValues() {
+    for (int i = 0; i < 51; ++i) {
+        double value = 0;
+        for (int j = 0; j < m_nTerms; ++j) {
+            value += (m_terms[j]->coeff() * pow(i - 25, m_terms[j]->degree()));
+        }
+        m_values[i] = value;
+    }
+}
+
+Term* Function::terms(int index) {
+    return m_terms[index];
+}
+
+double* Function::values() {
+    return m_values;
+}
+
+int Function::nTerms() {
+    return m_nTerms;
+}
+
+/*
+ plus sign if it's not the first term
+ coefficient if it's not 1
+ x if it's not to the 0 power
+ ^y if it's not to the 1 power
+*/
+
+void Function::displayFunction() {
+    for (int i = 0; i < m_nTerms; ++i) {
+        if (i != 0) {
+            cout << "+ ";
+        }
+        if (terms(i)->coeff() == 1 && terms(i)->degree() == 0) {
+            cout << "1";
+        }
+        if (terms(i)->coeff() != 1) {
+            cout << terms(i)->coeff();
+        }
+        if (terms(i)->degree() != 0) {
+            cout << "x";
+        }
+        if (terms(i)->degree() != 1 && terms(i)->degree() != 0) {
+            cout << "^" << terms(i)->degree();
+        }
+        cout << " ";
+    }
+    cout << endl;
+}
+
+/*
+ ====================================
+ Auxiliary functions and main routine
+ ====================================
+*/
 
 int main() {
-    char graph[51][51];
-    fillGraph(graph);
-    drawAxes(graph);
-    
-    int degree = 0;
-    int coefficients[10];
-    cout << "What is the degree of the function? ";
-    cin >> degree;
-    
-    getCoefficients(coefficients, degree);
-    draw(coefficients, degree, graph);
-}
-
-void fillGraph(char graph[][51]) {
-    for (int i = 0; i < 51; ++i) {
-        for (int j = 0; j < 51; ++j) {
-            graph[i][j] = ' ';
-        }
-    }
-}
-
-void drawAxes(char graph[][51]) {
-    // x-axis
-    for (int i = 1; i < 50; ++i) {
-        graph[25][i] = '-';
-    }
-    graph[25][0] = '<';
-    graph[25][50] = '>';
-    // y-axis
-    for (int j = 0; j < 51; ++j) {
-        graph[j][25] = '|';
-    }
-    graph[0][25] = '^';
-    graph[50][25] = 'v';
-}
-
-void getCoefficients(int coefficients[], int degree) {
-    for (int i = 0; i < degree; ++i) {
-        cout << "What is the coefficient of the term of degree " << i + 1 << "? ";
-        int co = 0;
-        cin >> co;
-        coefficients[i] = co;
-    }
-}
-
-void draw(int coefficients[], int degree, char graph[][51]) {
-    for (int i = 0; i < 51; ++i) {
-        int pos = 0;
-        for (int j = 0; j < degree; ++j) {
-            pos += coefficients[j] * pow(i - 25, j + 1);
-        }
-        if (pos < 26 && pos > -26) {
-            graph[25 - pos][i] = '*';
-        }
-    }
-    
-    for (int k = 0; k < 51; ++k) {
-        for (int l = 0; l < 51; ++l) {
-            cout << graph[k][l];
-        }
-        cout << endl;
-    }
+    Function f;
+    f.displayFunction();
 }
